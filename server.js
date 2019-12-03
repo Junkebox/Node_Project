4366c96827d750afe2bc00834dfa8a9248c64ea6
BrunoSc1703 <junkebox@gmail.com>
2019-12-03 12:23:24 +0800

    adding delete function

--- server.js
+++ server.js
@@ -27,10 +27,32 @@ app.get('/', (req, res) => {
     res.render('index', {contents: contents});
   });
 });
+app.get('/delete/:id', (req, res) => {
+  var rawdata = fs.readFileSync('CD-Data.json');
+  var student = JSON.parse(rawdata);
+  var urlname = url.parse(req.url,true).query;
 
+  if(urlname.studioname == 1) 
+    findAndRemove(student.AlbumStudio,'title',req.params.id);
+  else 
+    findAndRemove(student.RemixAlbum,'title',req.params.id);
+
+  let data = JSON.stringify(student);
+  fs.writeFileSync('CD-Data.json', data);
+  res.redirect('/');
+});
 
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
 });
 
+
+function findAndRemove(array, property, value) {
+  array.forEach(function(result, index) {
+    if(result[property] === value) {
+      //Remove from array
+      array.splice(index, 1);
+    }    
+  });
+}
